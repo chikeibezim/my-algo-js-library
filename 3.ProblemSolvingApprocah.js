432fd5928ab748d59bf9d56453954919b6ca6797
Chike Ibezim <ibezimchike@gmail.com>
2023-01-10 17:04:58 +0100

    Problem solving approach

---
+++ 3.ProblemSolvingApprocah.js
@@ -0,0 +1,48 @@
+/*
+    What is an Algorithm?
+    An algrorithm is a set of steps to accomplish a certain task
+
+    Almost everything you do in programminginvolves some kind of algorithm
+
+    How do you improve?
+        1. Devise a plan for solving problems
+        2. Master common problem solving patterns
+
+    Problem Solving
+    1. Understand the problem
+    2. Explore concrete examples
+    3. Break it down
+    4. Solve/Simplify
+    5. Look Back and Refactor
+
+
+    1. Understanding the Problem:
+        * Can i restate the problem in my own words? - simplify the problem into something you can relate to or understand
+        * What are the inputs that go ino the problem? - know the data types, input size e.t.c
+        * What are the outputs that should come from the solution to the problem? - what should be the output
+        * Can the outputs be determined from the inputs? In order words,
+          do i have aenough information to solve the problem? - check for constraints or lack of sufficient information to solve the problem
+        * How should I label the important pieces of data that are part of the problem?
+
+    2. Explore concrete examples
+        *Coming up with examples can help you understand the problem better
+        *Examples also provide sanity checks that your eventual solution works the way it's supposed to
+
+        -----
+        *Start with small examples
+        *Progress to more complex examples
+        *Explore examples with empty inputs
+        *Explore examples with Invalid inputs
+
+    3. Break it down:
+        *Explicitly write out the steps you want to take and communicate with the interviewer or colleague
+        *These steps can be written as comments in your code explaining the step by step process tou will take in solving the problem
+
+    4. Solve or Simplify:
+        If you can't solve the problem, then simplify the problem and solve it the way you can
+        Then you can incoperate the difficulty back in the solution if you can
+        * Simplify and write something and you can also ask questions when stuck but make sure you're on a right part
+
+    5. Look back and Refactor
+        
+*/
