2bf00dbff78d28496347cb98b74fe99a1bd2f871
Chike Ibezim <ibezimchike@gmail.com>
2023-02-01 13:33:36 +0100

    Sliding windows Pattern

---
+++ 7.SlidingWindowPattern.js
@@ -0,0 +1,14 @@
+/*
+    SLIDING WINDOW PATTERN
+    This pattern involves creating a window which can either be an array or
+    number from one posiiton to another.
+    Depending on a certain condition, the window either increases or closes (and
+    a new window is created)
+
+    Very useful for keeping track of a subset of data in an array/string e.t.c
+
+    An Example:
+    Write a funnction called maxSubarraySum which accepts an arrau of integers and a 
+    numver called n. the function should calculate the maximum sum of n consecutive
+    elements in te array.
+*/
